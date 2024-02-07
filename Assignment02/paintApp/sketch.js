1d4f63b92d2517985ef9a06d6353b74e99fd4f11
josephaycock <jaycoc4@lsu.edu>
2024-02-06 20:09:53 -0600

    commit
    Paint App Completed

--- Assignment02/paintApp/sketch.js
+++ Assignment02/paintApp/sketch.js
@@ -1,27 +1,47 @@
 let selectedColor;
 let colorPallete;
+let dragging = false;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1000, 800);
   colorPallete = [
     new ColorPallete(10,10, color('red')),
-    new ColorPallete(10,30, color('orange')),
-    new ColorPallete(10,50, color('yellow')),
-    new ColorPallete(10,70, color('green')),
-    new ColorPallete(10,90, color('cyan')),
-    new ColorPallete(10,110, color('blue')),
-    new ColorPallete(10,130, color('magenta')),
-    new ColorPallete(10,150, color('brown')),
-    new ColorPallete(10,170, color('white')),
-    new ColorPallete(10,190, color('black'))
+    new ColorPallete(10,35, color('orange')),
+    new ColorPallete(10,60, color('yellow')),
+    new ColorPallete(10,85, color('green')),
+    new ColorPallete(10,110, color('cyan')),
+    new ColorPallete(10,135, color('blue')),
+    new ColorPallete(10,160, color('magenta')),
+    new ColorPallete(10,185, color('#503C3C')),
+    new ColorPallete(10,210, color('white')),
+    new ColorPallete(10,235, color('black'))
   ];
 }
 
 function draw() {
-  background(220);
+  //background(220);
 
   for (let i=0; i < colorPallete.length; i++) {
     colorPallete[i].draw();
+  }
+}
+
+function mousePressed() {
+  for (let i=0; i < colorPallete.length; i++) {
+    if (colorPallete[i].contains(mouseX, mouseY)) {
+      selectedColor = colorPallete[i].fill;
+      break;
+    }
+  }
+}
+
+function mouseDragged() {
+  if (mouseX > 30 && mouseY > 30) {
+    dragging = true;
+    fill(selectedColor);
+    stroke(selectedColor);
+    strokeWeight(10);
+    line(mouseX, mouseY, pmouseX, pmouseY); 
   }
 }
 
@@ -34,6 +54,12 @@ class ColorPallete {
 
   draw() {
     fill(this.fill);
+    stroke(0);
+    strokeWeight(1);
     square(this.x, this.y, 20);
+  }
+
+  contains(x,y) {
+    return x > this.x && x < this.x + 20 && y > this.y && y < this.y + 20;
   }
 }
