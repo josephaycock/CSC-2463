545204128bb1ffa366077c17cf1531010e45c195
Joseph Aycock <josephfelixaycock@outlook.com>
2024-02-15 13:56:56 -0600

    commit

--- Assignment03/sketch.js
+++ Assignment03/sketch.js
@@ -13,9 +13,27 @@ function preLoad() {
   goldenSprite.spriteSheet = 'Assets/golden.png';
   redSprite.spriteSheet = 'Assets/red.png';
 
+  // Set Animation
   let animation = {
-    stand: { row: 0, frames: 1 }
+    stand: { row: 0, frames: 1 },
+    walkRight:{},
+    walkLeft:{}
   }
+
+  // Set Frame Delay 
+  limeSprite.anis.frameDelay = 8;
+  goldenSprite.anis.frameDelay = 8;
+  redSprite.anis.frameDelay = 8;
+
+  // Adds Animations
+  limeSprite.addAnis(animation);
+  goldenSprite.addAnis(animation);
+  redSprite.addAnis(animation);
+
+  // Initialize Sprite Animation
+  limeSprite.changeAni('stand');
+  goldenSprite.changeAni('stand');
+  redSprite.changeAni('stand');
 }
 
 function setup() {
@@ -24,4 +42,20 @@ function setup() {
 
 function draw() {
   background(220);
+}
+
+function walkRight() {
+
+}
+
+function walkLeft() {
+
+}
+
+function stop() {
+
+}
+
+function keyTyped() {
+  
 }
