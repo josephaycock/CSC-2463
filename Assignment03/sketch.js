cc1a50b023a53d58231ed3ce7c37f73ddb0f3e85
Joseph Aycock <josephfelixaycock@outlook.com>
2024-02-15 09:00:04 -0600

    commit

---
+++ Assignment03/sketch.js
@@ -0,0 +1,23 @@
+let limeSprite;
+let goldenSprite;
+let redSprite;
+
+function preLoad() {
+  // Create Sprite
+  limeSprite = new Sprite(200,200, 80, 80);
+  goldenSprite = new Sprite(200,200, 80, 80);
+  redSprite = new Sprite(200,200, 80, 80);
+
+  // Load Sprite Sheet
+  limeSprite.spriteSheet = "Assets/lime.png";
+  goldenSprite.spriteSheet = "Assets/golden.png";
+  redSprite.spriteSheet = "Assets/red.png";
+}
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
