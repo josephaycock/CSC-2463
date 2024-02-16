1c315c154a092e038c6124f64641f197c1df4d01
Joseph Aycock <josephfelixaycock@outlook.com>
2024-02-16 12:51:16 -0600

    commit

--- spriteAnimationPracticePart02/sketch.js
+++ spriteAnimationPracticePart02/sketch.js
@@ -1,5 +1,7 @@
 // Define sprite
 let sprite;
+// Character array
+let characters = [];
 
 // Define preLoad
 function preload() {
@@ -11,8 +13,8 @@ function preload() {
     walkDown: { row: 5, col: 6, frames: 6}
   };
 
-  let Character = new Character(100, 100, 80, 80, 'assets/cyclops.png', animations);
-  let Character2 = new Character(200, 200, 80, 80, 'assets/cyclops.png', animations);
+  characters.push(new Character(100, 100, 80, 80, 'assets/cyclops.png', animations));
+  characters.push(new Character(200, 200, 80, 80, 'assets/cyclops.png', animations));
 }
 
 function setup() {
@@ -22,36 +24,39 @@ function setup() {
 function draw() {
   background(0);
   
-  // Key press
-  if (kb.pressing('d')) {
-    walkRight();
-  } else if (kb.pressing('a')) {
-    walkLeft();
-  } else if (kb.pressing('w')) {
-    walkUp();
-  } else if (kb.pressing('s')) {
-    walkDown();
-  } else {
-    stop();
-  }
+  // forEach loop
+  characters.forEach((characters) => {
+    // Key press
+    if (kb.pressing('d')) {
+      characters.walkRight();
+    } else if (kb.pressing('a')) {
+      characters.walkLeft();
+    } else if (kb.pressing('w')) {
+      characters.walkUp();
+    } else if (kb.pressing('s')) {
+      characters.walkDown();
+    } else {
+      characters.stop();
+    }
 
-  // Update sprite position
-  if (sprite.x + sprite.width/4 > width) {
-    walkLeft();
-  } else if (sprite.x - sprite.width/4 < 0) {
-    walkRight();
-  } else if (sprite.y + sprite.height/4 > height) {
-    walkUp();
-  } else if (sprite.y - sprite.height/4 < 0) {
-    walkDown();
-  }
+    // Update sprite position
+    if (sprite.x + sprite.width/4 > width) {
+      walkLeft();
+    } else if (sprite.x - sprite.width/4 < 0) {
+      walkRight();
+    } else if (sprite.y + sprite.height/4 > height) {
+      walkUp();
+    } else if (sprite.y - sprite.height/4 < 0) {
+      walkDown();
+    }
+  });
 }
 
 // Character class
 class Character {
   constructor(x, y, width, height, spriteSheet, animations) {
     // Create sprite
-    this.sprite = new Sprite(x, y.width, width, height);
+    this.sprite = new Sprite(x, y, width, height);
     // Load sprite sheet
     this.sprite.spriteSheet = 'assets/cyclops.png';
     // Set frame delay
@@ -61,44 +66,44 @@ class Character {
     // Initialize sprite animation
     this.sprite.changeAni('walkRight');
   };
-}
 
-// walkRight function
-function walkRight() {
-  sprite.vel.x = 1;
-  sprite.vel.y = 0;
-  sprite.changeAni('walkRight');
-  // Flip sprite
-  sprite.scale.x = 1;
-}
+  // walkRight function
+  walkRight() {
+    this.sprite.vel.x = 1;
+    this.sprite.vel.y = 0;
+    this.sprite.changeAni('walkRight');
+    // Flip sprite
+    this.sprite.scale.x = 1;
+  }
 
-// walkLeft function
-function walkLeft() {
-  sprite.vel.x = -1;
-  sprite.vel.y = 0;
-  sprite.changeAni('walkRight');
-  // Flip sprite
-  sprite.scale.x = -1;
-}
+  // walkLeft function
+  walkLeft() {
+    this.sprite.vel.x = -1;
+    this.sprite.vel.y = 0;
+    this.sprite.changeAni('walkRight');
+    // Flip sprite
+    this.sprite.scale.x = -1;
+  }
 
-// walkUp function
-function walkUp() {
-  sprite.vel.y = -1;
-  sprite.vel.x = 0;
-  sprite.changeAni('walkUp');
-}
+  // walkUp function
+  walkUp() {
+    this.sprite.vel.y = -1;
+    this.sprite.vel.x = 0;
+    this.sprite.changeAni('walkUp');
+  }
 
-// walkDown function
-function walkDown() {
-  sprite.vel.y = 1;
-  sprite.vel.x = 0;
-  sprite.changeAni('walkDown');
-}
+  // walkDown function
+  walkDown() {
+    this.sprite.vel.y = 1;
+    this.sprite.vel.x = 0;
+    this.sprite.changeAni('walkDown');
+  }
 
-function stop() {
-  sprite.vel.x = 0;
-  sprite.vel.y = 0;
-  sprite.changeAni('stand');
+  stop() {
+    this.sprite.vel.x = 0;
+    this.sprite.vel.y = 0;
+    this.sprite.changeAni('stand');
+  }
 }
 
 // Define keyTyped function
